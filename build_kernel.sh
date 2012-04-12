08a278c401896d990751ca135f723d7fef7b7c8e
gc chang <ragepekr@gmail.com>
2012-04-12 23:14:32 +0900

    Patch_Source

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.4.3/bin/arm-eabi-
+export CROSS_COMPILE=/root/workspace/arm-eabi-4.4.3/bin/arm-eabi-
 export USE_SEC_FIPS_MODE=true
 
 make u1_kor_skt_defconfig
-make
+make -j4 CROSS_COMPILE=/root/workspace/arm-eabi-4.4.3/bin/arm-eabi- CONFIG_INITRAMFS_SOURCE=/root/workspace/kernel_repack/uc21_2nd_init_cwm
+
+cp drivers/net/wireless/bcmdhd/dhd.ko /root/workspace/kernel_repack/uc21_2nd_init_cwm/lib/modules/
+cp drivers/scsi/scsi_wait_scan.ko /root/workspace/kernel_repack/uc21_2nd_init_cwm/lib/modules/
+
+make -j4 zImage CROSS_COMPILE=/root/workspace/arm-eabi-4.4.3/bin/arm-eabi- CONFIG_INITRAMFS_SOURCE=/root/workspace/kernel_repack/uc21_2nd_init_cwm
+
