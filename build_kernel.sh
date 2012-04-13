d22b07ff25817787af52d57399748dd682548bd7
ragepekr <ragepekr@gmail.com>
2012-04-13 18:15:49 +0900

    mod

--- build_kernel.sh
+++ build_kernel.sh
@@ -5,10 +5,10 @@ export CROSS_COMPILE=/root/workspace/arm-eabi-4.4.3/bin/arm-eabi-
 export USE_SEC_FIPS_MODE=true
 
 make u1_kor_skt_defconfig
-make -j4 CROSS_COMPILE=/root/workspace/arm-eabi-4.4.3/bin/arm-eabi- CONFIG_INITRAMFS_SOURCE=/root/workspace/kernel_repack/uc21_2nd_init_cwm
+nice -n 10 make -j4 CROSS_COMPILE=/root/workspace/arm-eabi-4.4.3/bin/arm-eabi- CONFIG_INITRAMFS_SOURCE=/root/workspace/kernel_repack/uc21_2nd_init_reset
 
-cp drivers/net/wireless/bcmdhd/dhd.ko /root/workspace/kernel_repack/uc21_2nd_init_cwm/lib/modules/
-cp drivers/scsi/scsi_wait_scan.ko /root/workspace/kernel_repack/uc21_2nd_init_cwm/lib/modules/
+cp drivers/net/wireless/bcmdhd/dhd.ko /root/workspace/kernel_repack/uc21_2nd_init_reset/lib/modules/
+cp drivers/scsi/scsi_wait_scan.ko /root/workspace/kernel_repack/uc21_2nd_init_reset/lib/modules/
 
-make -j4 zImage CROSS_COMPILE=/root/workspace/arm-eabi-4.4.3/bin/arm-eabi- CONFIG_INITRAMFS_SOURCE=/root/workspace/kernel_repack/uc21_2nd_init_cwm
+nice -n 10 make -j4 zImage CROSS_COMPILE=/root/workspace/arm-eabi-4.4.3/bin/arm-eabi- CONFIG_INITRAMFS_SOURCE=/root/workspace/kernel_repack/uc21_2nd_init_reset
 
